ecd4a6a64e9229782837fb4680654396cd3892bf
RobC1528 <qracatapang01@tip.edu.ph>
2025-05-23 00:38:57 +0800

    appy.py

---
+++ appy.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import os
+
+st.title("Weather Image Classifier")
+
+# Debug
+st.write("Files in directory:", os.listdir())
+
+# Load model
+try:
+    model = load_model("cnn_best_model.keras")
+except Exception as e:
+    st.error(f"Failed to load model: {e}")
+    st.stop()
+
+class_names = ['cloudy', 'rain', 'shine', 'sunrise']
+
+uploaded_file = st.file_uploader("Upload a weather image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+
+    img = img.resize((150, 150))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    try:
+        prediction = model.predict(img_array)
+        predicted_class = class_names[np.argmax(prediction)]
+        st.markdown(f"### redicted Class: **{predicted_class.upper()}**")
+    except Exception as e:
+        st.error(f"Prediction failed: {e}")
